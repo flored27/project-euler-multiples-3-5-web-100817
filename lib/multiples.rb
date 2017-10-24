c8e578b136ca3690f82cb20c7f194f7207ce77eb
flored27 <dflores@fandm.edu>
2017-10-24 09:56:46 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,50 @@
 # Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(limit)
+  i = 3
+  container = []
+  while i < limit
+    if i%3 == 0 || i%5==0
+      container << i
+    end
+    i+=1
+  end
+  container
+end
+
+def sum_multiples(limit)
+  sum_arr = collect_multiples(limit)
+  sum_arr.inject{|sum, x| sum+x}
+end
+
+class Multiples
+
+  attr_accessor :limit, :container
+
+  def initialize(limit)
+    @limit = limit
+    @container = []
+    if limit == nil
+     "ArgumentError"
+    end
+  end
+
+  def collect_multiples
+    i = 3
+    while i < @limit
+      if i%3 == 0 || i%5==0
+        @container << i
+      end
+      i+=1
+    end
+    @container
+  end
+
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+
+end
